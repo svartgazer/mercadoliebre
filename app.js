8d819d78464a49dc222d42f3abc60f966a83e0ba
svartgazer <hola.sergioruiz@gmail.com>
2021-11-25 14:09:35 -0600

    agregue procfile

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(4000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT || 3000, function (){
+    console.log('Servidor corriendo en el puerto 3000');
 });
 
 app.get('/', (req,res)=>{
